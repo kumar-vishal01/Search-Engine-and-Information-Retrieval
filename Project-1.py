78423e5ae2ac9fdb458c3d5dcfc4b3691f2838eb
Vishal Kumar <vishalk0273@gmail.com>
2026-02-22 23:13:55 +0530

    SEIR

--- Project-1.py
+++ Project-1.py
@@ -1,8 +1,12 @@
+'''Python Project
+Write a python program that takes a URL on the command line, fetches the page, and outputs (one per line)
+Page Title (without any HTML tags)
+Page Body (just the text, without any html tags)
+All the URLs that the page points/links to'''
 
 import sys
 import requests
 from bs4 import BeautifulSoup
-
 
 if (len(sys.argv) < 2):
     print("Invalid Command, add website.")
@@ -12,31 +16,33 @@ url = sys.argv[1]
 
 try:
     response = requests.get(url)
-
     if(response.status_code != 200):
         print("Unable to fetch webpage.")
         sys.exit()
-    
     print(response)
-
+    
 except request.RequestException:
     print("Failed to fetch webpage.")
     sys.exit()
 
 html_content = response.text
-
+# soup
 soup = BeautifulSoup(html_content, 'html.parser')
-
 print("\n")
+# parse title
 title_tag = soup.find('title')
 if title_tag:
     print("Title of the webpage - ",title_tag.get_text())
 else:
     print("No Title Found")
-
+    
 print("\n")
-print("Body of page - \n ", soup.get_text())
-
+# parse body
+if soup.body:
+    print("Body of page - \n ", soup.get_text())
+else:
+    print("No Body Found")
+# parse links
 print("Links present on the webpage - ")
 links = soup.find_all('a')
 if links:
@@ -44,4 +50,5 @@ if links:
         print(link.get('href'))
 else:
     print("No links found")
+
 
